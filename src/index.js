b3be14838e7c99abb5f3ee012be1474eef115350
BuildTools <unconfigured@null.spigotmc.org>
2019-05-08 14:49:19 +1000

    Tryign to get component built

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Home from "./view";
+import CountrySelect from "./countries";
+import PhoneNumber from "./number";
+import "./base.css";
 
-import "./styles.css";
+export default class PhoneInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      country: {}
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    this.selectCountry = this.selectCountry.bind(this);
+    this.onValid = this.onValid.bind(this);
+  }
+
+  selectCountry(country) {
+    this.setState({ country: country });
+  }
+
+  onValid(phoneNumber) {
+    this.setState({ final: phoneNumber });
+    this.props.onChange(phoneNumber);
+  }
+
+  render() {
+    return (
+      <div className="accessible-phone-input">
+        <CountrySelect onchange={this.selectCountry} />
+        <div className="accessible-form-group international-dial-code">
+          <label>Int.</label>
+          <span aria-label="International dial code">
+            {this.state.country.dialCode}
+          </span>
+        </div>
+        <PhoneNumber
+          ISOCode={this.state.country.ISOCode}
+          dialCode={this.state.country.dialCode}
+          onValid={this.onValid}
+        />
+      </div>
+    );
+  }
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
